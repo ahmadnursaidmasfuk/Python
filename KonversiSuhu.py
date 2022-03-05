831a53f25133356f666797d62f9329490bf3dbc9
ansamas <89480712+ansamas@users.noreply.github.com>
2022-03-05 16:07:20 +0700

    Add files via upload
    Progam konversi suhu menggunakan OOP (object Oriented Programming) dengan bahasa pemrograman python. Progam ini dapat mengkonversi suhu celsius, kelvin, rearmur dan farenheit sesuai input yang dimasukkan

---
+++ KonversiSuhu.py
@@ -0,0 +1,85 @@
+class Suhu :
+    def __init__(self,name):
+        self.name=name
+   
+    def keterangan(self):
+        return self.name
+   
+    def konversi(self, order1, order2):
+        
+        if order1 == 0 and order2 == 1:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=4/5*suhu_awal
+        
+        elif order1 == 0 and order2 == 2:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=9/5*suhu_awal+32
+
+        elif order1 == 0 and order2 == 3:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=suhu_awal+273
+        
+        elif order1 == 1 and order2 == 0:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=5/4*suhu_awal
+        
+        elif order1 == 1 and order2 == 2:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=9/4*suhu_awal+32
+
+        elif order1 == 1 and order2 == 3:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=9/5*suhu_awal+273
+
+        elif order1 == 2 and order2 == 0:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=(suhu_awal-32)*5/9
+        
+        elif order1 == 2 and order2 == 1:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=(suhu_awal-32)*4/9
+
+        elif order1 == 2 and order2 == 3:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=(suhu_awal-32+273)*5/9
+        
+        elif order1 == 3 and order2 == 0:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=suhu_awal-273
+        
+        elif order1 == 3 and order2 == 1:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=(suhu_awal-273)*4/5
+
+        elif order1 == 3 and order2 == 2:
+            suhu_awal=float(input("Masukkan nilai suhu: "))
+            suhu_akhir=(suhu_awal-273+32)*9/5
+
+        return suhu_akhir
+
+
+suhu_item1=Suhu("Celcius")
+suhu_item2=Suhu("Reamur")
+suhu_item3=Suhu("Farenheit")
+suhu_item4=Suhu("Kelvin")
+
+suhu_items=[suhu_item1,suhu_item2,suhu_item3,suhu_item4]
+
+index=0
+for suhu_item in suhu_items :
+    print(index, " satuan suhu: ", suhu_item.keterangan())
+    index+=1
+print("-------------------")
+
+order1=int(input("silahkan masukkan nomer satuan yang akan dikonversi: "))
+select_suhu1=suhu_items[order1]
+order2=int(input("silahkan masukkan nomer satuan tujuan dikonversi: "))
+select_suhu2=suhu_items[order2]
+print("satuan tujuan dikonversi adalah: ", select_suhu1.name)
+print("satuan yang akan dikonversi adalah: ", select_suhu2.name)
+
+print("-------------------")
+
+
+hasil=select_suhu1.konversi(order1,order2)
+print("Hasil konversi suhu dari ",select_suhu1.name, " menjadi ", select_suhu2.name, " adalah: ",hasil)
